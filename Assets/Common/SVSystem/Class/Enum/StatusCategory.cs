88186f9397ac600939052ecc6ad202db6eb11c6f
wtr <w.inou.24.8.1574@4nonome.com>
2026-09-12 20:45:50 +0900

    add StatusCategory and StatusMethod
    どのようなパラメータなのかを決めるStatusCategoryとその値をどのように扱うStatusMethodをenum型として定義した。

---
+++ Assets/Common/SVSystem/Class/Enum/StatusCategory.cs
@@ -0,0 +1,9 @@
+public enum StatusCategory
+{
+    HP = 0,
+    Attack = 1,
+    Magic = 2,
+    Defense = 3,
+    Speed = 4,
+    Count = 5
+}
---
+++ Assets/Common/SVSystem/Class/Enum/StatusMethod.cs
@@ -0,0 +1,7 @@
+public enum StatusMethod
+{
+    Base = 0,
+    Add = 1,
+    Multiply = 2,
+    Count = 3
+}
