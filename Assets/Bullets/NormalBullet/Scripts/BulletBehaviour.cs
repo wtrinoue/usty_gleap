bec9af5e3a46ef3e65134305eb625279cea5201b
wtr <w.inou.24.8.1574@4nonome.com>
2026-09-24 19:37:43 +0900

    repair prefabs
    StatusContainerを付けたはずのPrefabがなぜかこわれていたので修正した。どうやらUnity内で用いられているquidの副作用から生じているらしく、個々の開発中にエラーを無視して開発していたので別のデバイスでエラーになっていたらしい。

--- Assets/Bullets/NormalBullet/Scripts/BulletBehaviour.cs
+++ Assets/Bullets/NormalBullet/Scripts/BulletBehaviour.cs
@@ -3,19 +3,15 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     [SerializeField] private float lifeTimeSeconds = 5f; // 生存時間（秒）
-    private StatusActionHolder statusActionHolder;
     private StatusContainer statusContainer;
-    private TargetStatusAction attackAction;
     private Rigidbody2D rb;
     private float lifeTimer;
     private bool isDestroyed;
 
     void Awake()
     {
-        statusActionHolder = GetComponent<StatusActionHolder>();
         statusContainer = GetComponent<StatusContainer>();
         rb = GetComponent<Rigidbody2D>();
-        attackAction = statusActionHolder.GetTargetStatusActionFromIndex(0);
 
         // 寿命カウントをコルーチンで開始
         StartCoroutine(LifeTimeRoutine());
